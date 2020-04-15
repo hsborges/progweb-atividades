4241d8223f48557ade87adb7923cd7dbe17b7db5
Hudson Silva Borges <hudsonsilbor@gmail.com>
2020-04-14 22:55:11 -0400

    exercicio para criar um perfil

---
+++ PaginaGradweb/index.template.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MEU_NOME - Perfil</title>
+  <link rel="stylesheet" href="estilos.css">
+</head>
+
+<body>
+  <div class="conteudo">
+    <div class="foto"><img src="foto.png" alt=""></div>
+    <div class="apresentacao">
+      <span class="nome">MEU_NOME</span>
+      <span class="curso">MEU_CURSO (NIVEL)</span>
+      <span class="email">MEU_EMAIL</span>
+    </div>
+  </div>
+  <footer>
+    <!-- Insira aqui o texto abaixo substituindo pelo conteudo correto -->
+    <!-- Texto: Página gerada em dia/mes/ano as hora:minuto:segundo -->
+    <!-- Você deverá usar PHP e configurar para o timezone de Campo Grande/MS -->
+  </footer>
+</body>
+
+</html>
